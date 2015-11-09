07696055bad48119200828a525f78cd200d3ccdf
sonnv_56 <sonnv_56@vnu.edu.vn>
2015-11-09 18:18:37 +0700

    update CIA

--- com/fit/ducanhTest/JdtParserTest.java
+++ com/fit/ducanhTest/JdtParserTest.java
@@ -6,21 +6,21 @@ import com.fit.jdtParser.ClassFileParser;
 
 public class JdtParserTest {
 	public static void main(String[] args) {
-		String classPath = "C:\\Users\\DucAnh\\Dropbox\\Project\\J2EE\\DEMO J2EE 2\\dukes-forest\\dukes-forest\\dukes-payment\\src\\java\\com\\forest\\payment\\services\\Payment.java";
+		String classPath = "C://Users//son//Desktop//Workspace//CIA//dukes-forest//dukes-store//src//java//com//forest//web//CustomerController.java";
 		ClassFileParser classFileParser = new ClassFileParser(classPath);
 
 		// display all annotation
-		for (ASTNode annotation : classFileParser.getListAnnotation()) {
-			System.out.println(annotation.toString());
-		}
+//		for (ASTNode annotation : classFileParser.getListAnnotation()) {
+//			System.out.println(annotation.toString());
+//		}
 
 		// display all field
 		for (ASTNode annotation : classFileParser.getListFieldDeclaration()) {
 			System.out.println(annotation.toString());
 		}
-		// display all method
-		for (ASTNode annotation : classFileParser.getListMethodDeclaration()) {
-			System.out.println(annotation.toString());
-		}
+//		// display all method
+//		for (ASTNode annotation : classFileParser.getListMethodDeclaration()) {
+//			System.out.println(annotation.toString());
+//		}
 	}
 }
