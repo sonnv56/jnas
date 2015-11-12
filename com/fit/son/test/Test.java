b34af3f8d35b33dc2682e9ca6a43df7a66470c44
Son <Son>
2015-11-12 20:33:44 +0700

    conflig

--- com/fit/son/test/Test.java
+++ com/fit/son/test/Test.java
@@ -1,5 +1,8 @@
 package com.fit.son.test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.eclipse.jdt.core.dom.ASTNode;
 
 import com.fit.jdtParser.ClassFileParser;
