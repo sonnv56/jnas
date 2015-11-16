adc942278faf1c8a4799e571632398a9eb37459e
sonnv_56 <sonnv_56@vnu.edu.vn>
2015-11-16 11:19:01 +0700

    fix find injection points bugs

--- com/fit/process/cdi/CDIProcessor2.java
+++ com/fit/process/cdi/CDIProcessor2.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 import org.eclipse.jdt.core.dom.FieldDeclaration;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
 
 import com.fit.jdtParser.ClassFileParser;
 import com.fit.loader.ProjectLoader;
 import com.fit.loader.tree.Search;
+import com.fit.object.ClassNode;
 import com.fit.object.InjectionPoint;
 import com.fit.object.Node;
 import com.fit.object.ProjectNode;
@@ -24,29 +26,38 @@ public class CDIProcessor2 {
 	private ProjectNode projectNode;
 
 	public static void main(String[] args) {
-//		String projectRootPath = "C://Users//son//Downloads//dukes-forest//dukes-forest";
+		String projectRootPath = "C://Users//son//Downloads//dukes-forest//dukes-forest";
 //		String projectRootPath = "C://Users//Chicky//Documents//NetBeansProjects//CIASample";
-		String projectRootPath = "C://Users//Chicky//Desktop//Workspace//Project//CIA//javaee-tutorial-6//examples//case-studies//dukes-forest";
+//		String projectRootPath = "C://Users//Chicky//Desktop//Workspace//Project//CIA//javaee-tutorial-6//examples//case-studies//dukes-forest";
 		ProjectNode projectNode = ProjectLoader.load(projectRootPath);
 		CDIProcessor2 processor = new CDIProcessor2();
 		processor.setProjectNode(projectNode);
 
 		processor.process();
-
+		
 	}
 	
 	public void process(){
 		List<Node> namedNodes = Search.searchNode(projectNode, new CDINamedCondition());
 		List<InjectionPoint> points = findInjectionPointsInInjector(namedNodes);
-		updateInjectionPointsWithInjectee(points);
+		points = updateInjectionPointsWithInjectee(points);
 		updateProjectNode();
+		for (InjectionPoint point : points) {
+//			
+//			System.out.println(point.getStatement());
+//			
+			System.out.println(point.getInjectee().getPath());
+//			System.out.println("-------");
+		}
 	}
 	private void updateProjectNode() {
 		
 		
 	}
-	private void updateInjectionPointsWithInjectee(List<InjectionPoint> points) {
-		for (InjectionPoint injectionPoint : points) {
+	private List<InjectionPoint> updateInjectionPointsWithInjectee(List<InjectionPoint> points) {
+		List<InjectionPoint> result = new ArrayList<InjectionPoint>();
+		for (int i =  0; i < points.size(); i++) {
+			InjectionPoint injectionPoint  = points.get(i);
 			String statement = injectionPoint.getStatement();
 			int index = statement.indexOf(INJEXTION_ANOTATION);
 			if(index!=-1){										//Xac dinh co phai su dung @Injection
@@ -55,7 +66,6 @@ public class CDIProcessor2 {
 				//Lay qualifier
 				String qualifier = statement.substring(index, index2);
 				if(qualifier.indexOf("@") != -1){				//Neu la qualifier
-					System.out.println(qualifier);
 					//Bao qualifier o class
 					List<Node> qualifiers = Search.searchNode(projectNode,new CDICustomQualifierCondition(qualifier));
 					//Khai bao qualifier bang @Produces
@@ -67,36 +77,53 @@ public class CDIProcessor2 {
 						qualifiers = Search.searchNode(projectNode,new CDIObservesCondition(qualifier));
 					}
 					//Xac dinh chinh xac (truong hop co nhieu qualifiers giong nhau)
-					injectionPoint.setInjectee(getExactNode(qualifiers));
+					Node node = getExactNode(qualifiers);
+					injectionPoint.setInjectee(node);
+					result.add(injectionPoint);
 				}
 			}
 		}
+		return result;
 	}
-
+	/**
+	 * Xu ly truong hop tim chinh xac vi tri cua injectee
+	 * @param qualifiers
+	 * */
 	private Node getExactNode(List<Node> qualifiers) {
 		if(qualifiers.size() > 0){
 			return qualifiers.get(0);
 		}
-		return null;
+		return new ClassNode();
 	}
-
+	/**
+	 * Tim cac injection point trong injector
+	 * @param namedNodes injector
+	 * */
 	private List<InjectionPoint> findInjectionPointsInInjector(List<Node> namedNodes) {
 		List<InjectionPoint> points = new ArrayList<InjectionPoint>();
 		for (Node node : namedNodes) {
 			ClassFileParser classFileParser = new ClassFileParser(node.getPath());
 			// Kiem tra cac field
 			for (FieldDeclaration field : classFileParser.getListFieldDeclaration()) {
-				System.out.println(field.toString());
+//				System.out.println(field.toString());
 				if(field.toString().trim().indexOf(INJEXTION_ANOTATION)!=-1){
-					
 					points.add(createAInjectionPoint(node, field.toString().trim()));
 				}
 			}
 			// Kiem tra cac method
 			for (MethodDeclaration method : classFileParser .getListMethodDeclaration()) {
-				if(method.toString().trim().indexOf(INJEXTION_ANOTATION)!=-1 
-						|| method.toString().trim().indexOf(PRODUCES_ANOTATION)!=-1){
+				if(method.toString().trim().indexOf(INJEXTION_ANOTATION)!=-1 ){
 					points.add(createAInjectionPoint(node, method.toString().trim()));
+				}else{
+					if(method.toString().trim().indexOf(PRODUCES_ANOTATION)!=-1){
+						List<SingleVariableDeclaration> parameters = method .parameters();
+						for (SingleVariableDeclaration p : parameters) {
+							if (p.toString().indexOf("@")==-1) {
+								points.add(createAInjectionPoint(node, method.toString().trim()));
+								break;
+							}
+						}
+					}
 				}
 			}
 		}
