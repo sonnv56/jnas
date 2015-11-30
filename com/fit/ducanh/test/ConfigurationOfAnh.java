fee776efe66a35b703dd335f1e54f37dfb489f70
ducanhnguyen <anhnd.56@gmail.com>
2015-11-30 17:51:59 +0700

    update configuration of Duc Anh

--- com/fit/ducanh/test/ConfigurationOfAnh.java
+++ com/fit/ducanh/test/ConfigurationOfAnh.java
@@ -6,9 +6,10 @@ public interface ConfigurationOfAnh {
 	public static final String JSF_JPACRUD = "C:\\Users\\DucAnh\\Dropbox\\Workspace\\JSF test\\JsfJpaCrud";
 	public static final String MULTIPLE_CONFIG_JSF = "C:\\Users\\DucAnh\\Documents\\NetBeansProjects\\MultipleConfigJSF";
 	public static final String MULTIPLE_CONFIG_JSF2 = "G:\\Dropbox\\Workspace\\JSF test\\MultipleConfigJSF";
-	
+
 	public static final String REMOVE_COMMENT_TEST2 = "C:\\Users\\DucAnh\\Dropbox\\Workspace\\Eclipse project\\Jnas\\Remove Comment Test\\test2.java";
 	public static final String REMOVE_COMMENT_TEST1 = "C:\\Users\\DucAnh\\Dropbox\\Workspace\\Eclipse project\\Jnas\\Remove Comment Test\\test1.txt";
-	public static final String JSON_DUKES_FOREST_PATH2= "G:\\Dropbox\\Workspace\\JSF test\\dukes-forest.json";
+	public static final String JSON_DUKES_FOREST_PATH2 = "G:\\Dropbox\\Workspace\\JSF test\\dukes-forest.json";
 	public static final String JSON_DUKES_FOREST_PATH = "C:\\Users\\DucAnh\\Dropbox\\Workspace\\JSF test\\dukes-forest.json";
+	public static final String DEFAULT_JSON_PATH_FOR_DEPENDENCY_GRAPH = "C:\\Users\\DucAnh\\Dropbox\\Workspace\\Eclipse project\\Jnas\\src\\com\\fit\\view\\data\\default\\objects.json";
 }
