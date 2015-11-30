b57ecbb2a1f137b10a99bb8f8a261336fa8879bc
ducanhnguyen <anhnd.56@gmail.com>
2015-11-30 15:50:33 +0700

    remove comments

--- com/fit/process/DependencyGeneration.java
+++ com/fit/process/DependencyGeneration.java
@@ -1,30 +1,32 @@
 package com.fit.process;
 
+import java.util.List;
+
 import com.fit.object.Node;
 import com.fit.object.ProjectNode;
+import com.fit.process.cdi.CDIProcessor;
+import com.fit.process.jsf.JSFParser;
+import com.fit.process.jsf.TreeUpdater;
+import com.fit.process.jsf.object.Dependency;
 import com.fit.process.tostring.IToString;
-import com.fit.process.tostring.JsonStrategyForHung;
 import com.fit.process.tostring.TreeStrategy;
 import com.fit.process.ws.WebServiceProcessor;
 
 public class DependencyGeneration {
 	public static void parse(Node projectRootNode) {
-		
-		/** JSF parser here */
-		
-//		List<Dependency> dependenciesList = new JSFParser(projectRootNode).getDependenciesList();
-//		TreeUpdater.createConnection(dependenciesList);
-//		/** CDI parser here */
-//		CDIProcessor processor = new CDIProcessor();
-//		processor.setProjectNode((ProjectNode) projectRootNode);
 
-//		processor.process();
+		/** JSF parser here */
+
+		List<Dependency> dependenciesList = new JSFParser(projectRootNode).getDependenciesList();
+		TreeUpdater.createConnection(dependenciesList);
+		/** CDI parser here */
+		CDIProcessor processor = new CDIProcessor();
+		processor.setProjectNode((ProjectNode) projectRootNode);
+
+		processor.process();
+
 		/** Web service parser here */
-		
 		WebServiceProcessor wsp = new WebServiceProcessor((ProjectNode) projectRootNode);
 		wsp.process();
-		IToString displayer = new TreeStrategy(projectRootNode);
-		System.out.println(displayer.getString());
-
 	}
 }
