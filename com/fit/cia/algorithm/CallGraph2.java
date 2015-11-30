5e35a620bcd6fffa0a0769ed5f3356266d894ab1
ducanhnguyen <anhnd.56@gmail.com>
2015-11-30 15:47:28 +0700

    complete Wave algorithm

---
+++ com/fit/cia/algorithm/CallGraph2.java
@@ -0,0 +1,216 @@
+package com.fit.cia.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jgrapht.alg.DijkstraShortestPath;
+import org.jgrapht.graph.AsUndirectedGraph;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.ListenableDirectedGraph;
+
+import com.fit.object.Node;
+
+public class CallGraph2 {
+	public static class MyEdge extends DefaultWeightedEdge {
+		@Override
+		public String toString() {
+			return "";
+		}
+	}
+
+	private ArrayList<Node> nodes;
+	private ArrayList<Node> changeSetInNode;
+	private ArrayList<String> changeSet;
+	private ArrayList<String> core;
+	private ArrayList<String> impactSet;
+
+	private int depth = 1;
+	public ListenableDirectedGraph<String, MyEdge> graph;
+
+	public CallGraph2() {
+		nodes = new ArrayList<Node>();
+		changeSet = new ArrayList<String>();
+		changeSetInNode = new ArrayList<Node>();
+		core = new ArrayList<String>();
+		impactSet = new ArrayList<String>();
+
+		graph = new ListenableDirectedGraph<String, MyEdge>(MyEdge.class);
+
+		genarateGraph();
+	}
+
+	public CallGraph2(List<Node> _nodes, List<Node> _changeSet) {
+		nodes = (ArrayList<Node>) _nodes;
+		changeSetInNode = (ArrayList<Node>) _changeSet;
+		changeSet = new ArrayList<String>();
+		for (Node node : changeSetInNode) {
+			changeSet.add(node.getNodeName());
+		}
+		core = new ArrayList<String>();
+		impactSet = new ArrayList<String>();
+
+		graph = new ListenableDirectedGraph<String, MyEdge>(MyEdge.class);
+
+		genarateGraph();
+
+		System.out.println("num of Nodes: " + graph.vertexSet().size());
+		System.out.println("num of call: " + graph.edgeSet().size());
+	}
+
+	private void genarateGraph() {
+		String[] nodestring = new String[nodes.size()];
+		for (int i = 0; i < nodes.size(); i++) {
+			nodestring[i] = nodes.get(i).getNodeName();
+			graph.addVertex(nodestring[i]);
+		}
+		for (int i = 0; i < nodes.size(); i++) {
+			if (nodes.get(i).getCallees().size() > 0) {
+				for (Node callee : nodes.get(i).getCallees()) {
+					for (int j = 0; j < nodes.size(); j++) {
+						if (nodestring[j].equals(callee.getNodeName())) {
+							graph.addEdge(nodestring[i], nodestring[j]);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	public int distance(Node n1, Node n2) {
+		AsUndirectedGraph<String, MyEdge> unDirectedGraph = new AsUndirectedGraph<String, MyEdge>(graph);
+		List<MyEdge> path = DijkstraShortestPath.findPathBetween(unDirectedGraph, n1.getNodeName(), n2.getNodeName());
+		return path.size();
+	}
+
+	public int distance(String n1, String n2) {
+		AsUndirectedGraph<String, MyEdge> unDirectedGraph = new AsUndirectedGraph<String, MyEdge>(graph);
+		List<MyEdge> path = DijkstraShortestPath.findPathBetween(unDirectedGraph, n1, n2);
+		if (path != null)
+			return path.size();
+		return 10000;
+	}
+
+	public ArrayList<String> getNeighborSet(String vertex) {
+		ArrayList<String> neighbor = new ArrayList<String>();
+		for (String v : graph.vertexSet()) {
+			if (distance(v, vertex) <= depth) {
+				neighbor.add(v);
+			}
+		}
+		return neighbor;
+	}
+
+	public void caculateCore() {
+		core.clear();
+		ArrayList<String> tempSet = new ArrayList<String>();
+		for (String v : graph.vertexSet()) {
+			if (!changeSet.contains(v)) {
+				ArrayList<String> neighborSet = getNeighborSet(v);
+				ArrayList<String> neighborCSSet = intersection(neighborSet, changeSet);
+				if (neighborCSSet.size() > 1) {
+					tempSet.add(v);
+				}
+			}
+		}
+		core.addAll(changeSet);
+		core.addAll(tempSet);
+	}
+
+	public void calculateImpactSet() {
+		impactSet.clear();
+		caculateCore();
+		ArrayList<String> tempSet = new ArrayList<String>();
+		impactSet.addAll(core);
+		for (String m : graph.vertexSet()) {
+			if (!impactSet.contains(m)) {
+				ArrayList<String> callerSet = getCallerSet(m);
+				ArrayList<String> calleeSet = getCalleeSet(m);
+				if (intersection(impactSet, calleeSet).size() > 0 && intersection(impactSet, callerSet).size() > 0) {
+					tempSet.add(m);
+				}
+			}
+		}
+		impactSet.addAll(tempSet);
+	}
+
+	private <T> ArrayList<T> intersection(ArrayList<T> list1, ArrayList<T> list2) {
+		ArrayList<T> list = new ArrayList<T>();
+
+		for (T t : list1) {
+			if (list2.contains(t)) {
+				list.add(t);
+			}
+		}
+
+		return list;
+	}
+
+	private Node getMethodNode(String label) {
+		for (Node m : nodes) {
+			if (label.equals(m.getNodeName())) {
+				return m;
+			}
+		}
+		return null;
+	}
+
+	private ArrayList<String> getCallerSet(String method) {
+		ArrayList<String> callerSet = new ArrayList<String>();
+		Node node = getMethodNode(method);
+		ArrayList<Node> callerNode = new ArrayList<Node>();
+		callerNode.addAll(node.getCallers());
+		for (Node caller : callerNode) {
+			callerSet.add(caller.getNodeName());
+		}
+		return callerSet;
+	}
+
+	private ArrayList<String> getCalleeSet(String method) {
+		ArrayList<String> calleeSet = new ArrayList<String>();
+		Node node = getMethodNode(method);
+		ArrayList<Node> calleeNode = new ArrayList<Node>();
+		calleeNode.addAll(node.getCallees());
+		for (Node callee : calleeNode) {
+			calleeSet.add(callee.getNodeName());
+		}
+		return calleeSet;
+	}
+
+	public String changeSetToString() {
+		String ret = new String();
+		ret += changeSet.size() + " nodes in Change Set: \n";
+		for (String n : changeSet) {
+			ret += (n + "\n");
+		}
+		return ret;
+	}
+
+	public String impactSetToString() {
+		String ret = new String();
+		ret += impactSet.size() + " nodes in Impact Set: \n";
+		for (String n : impactSet) {
+			ret += (n + "\n");
+		}
+		return ret;
+	}
+
+	public void addToChangeSet(int nodeId) {
+		for (Node node : nodes) {
+			if (node.getId() == nodeId) {
+				changeSet.add(node.getNodeName());
+				return;
+			}
+		}
+	}
+
+	public void createRandomChangeSet() {
+		ArrayList<String> vertexes = new ArrayList<String>();
+		vertexes.addAll(graph.vertexSet());
+		int numOfMethod = vertexes.size();
+		System.out.println("numOfMethod: " + numOfMethod);
+		addToChangeSet(161);
+		addToChangeSet(390);
+		addToChangeSet(401);
+		addToChangeSet(351);
+	}
+}
--- com/fit/cia/algorithm/WaveAlgorithm.java
+++ com/fit/cia/algorithm/WaveAlgorithm.java
@@ -3,15 +3,17 @@ package com.fit.cia.algorithm;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import org.jgrapht.ext.JGraphXAdapter;
+
 import com.fit.cia.tree.TreeGeneration;
 import com.fit.ducanh.test.ConfigurationOfAnh;
-import com.fit.loader.ProjectLoader;
 import com.fit.object.Node;
-import com.fit.object.ProjectNode;
-import com.fit.process.DependencyGeneration;
-import com.fit.process.tostring.IToString;
-import com.fit.process.tostring.JsonStrategyForHung;
-import com.fit.util.Utils;
+import com.mxgraph.layout.mxFastOrganicLayout;
+import com.mxgraph.layout.mxIGraphLayout;
+import com.mxgraph.swing.mxGraphComponent;
 
 /**
  * 
@@ -20,20 +22,45 @@ import com.fit.util.Utils;
  */
 public class WaveAlgorithm implements IComputeImpactSet {
 	/** Danh sach cac nodes */
-	private List<Node> nodes_ = new ArrayList<>();
+	private static List<Node> nodes_ = new ArrayList<>();
 	/** Danh sach tap change set */
-	private List<Node> changeSet_ = new ArrayList<>();
+	private static List<Node> changeSet_ = new ArrayList<>();
+
+	private static void createAndShowGui() {
+		CallGraph cg = new CallGraph(nodes_, changeSet_);
+		cg.createRandomChangeSet();
+		cg.calculateImpactSet();
+		// System.out.println(cg.changeSetToString());
+		// System.out.println(cg.impactSetToString());
+
+		JFrame frame = new JFrame("DemoGraph");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JGraphXAdapter<String, com.fit.cia.algorithm.CallGraph.MyEdge> graphAdapter = new JGraphXAdapter<String, com.fit.cia.algorithm.CallGraph.MyEdge>(
+				cg.graph);
+
+		mxIGraphLayout layout = new mxFastOrganicLayout(graphAdapter);
+		layout.execute(graphAdapter.getDefaultParent());
+
+		frame.add(new mxGraphComponent(graphAdapter));
+
+		frame.pack();
+		frame.setLocationByPlatform(true);
+		frame.setVisible(true);
+	}
 
 	public static void main(String[] args) {
 		// Load project
-		ProjectNode projectRootNode = ProjectLoader.load(ConfigurationOfAnh.JSF_DUKES_FOREST_PATH);
+		// ProjectNode projectRootNode =
+		// ProjectLoader.load(HungConfig.JSF_DUKES_FOREST_PATH);
 
 		// Xay dung su phu thuoc
-		DependencyGeneration.parse(projectRootNode);
+		// DependencyGeneration.parse(projectRootNode);
 
 		// Xuat cay cau truc sang JSON
-		IToString displayer = new JsonStrategyForHung(projectRootNode);
-		Utils.writeContentToFile(displayer.getString(), ConfigurationOfAnh.JSON_DUKES_FOREST_PATH);
+		// IToString displayer = new JsonStrategy(projectRootNode);
+		// Utils.writeContentToFile(displayer.getString(),
+		// HungConfig.JSON_DUKES_FOREST_PATH);
 
 		// Phan tich JSON xay dung tree
 		TreeGeneration generator = new TreeGeneration(ConfigurationOfAnh.JSON_DUKES_FOREST_PATH);
@@ -42,6 +69,11 @@ public class WaveAlgorithm implements IComputeImpactSet {
 		// Wave (do something here)
 		List<Node> changeSet = new ArrayList<>();// add nodes later
 		WaveAlgorithm wave = new WaveAlgorithm(nodes, changeSet);
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGui();
+			}
+		});
 	}
 
 	public WaveAlgorithm(List<Node> nodes, List<Node> changeSet) {
@@ -53,5 +85,11 @@ public class WaveAlgorithm implements IComputeImpactSet {
 	@Override
 	public void compute() {
 		// do something else
+		CallGraph cg = new CallGraph(nodes_, changeSet_);
+		cg.createRandomChangeSet();
+		cg.calculateImpactSet();
+		System.out.println(cg.changeSetToString());
+		System.out.println(cg.coreToString());
+		System.out.println(cg.impactSetToString());
 	}
 }
