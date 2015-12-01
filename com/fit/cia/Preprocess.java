6c3c73e00afbaf70ac763de4a04d85fd8f8c8b02
ducanhnguyen <anhnd.56@gmail.com>
2015-12-01 10:16:08 +0700

    add Wave algorithm

--- com/fit/cia/Preprocess.java
+++ com/fit/cia/Preprocess.java
@@ -1,7 +1,12 @@
 package com.fit.cia;
 
+import java.util.List;
+
+import com.fit.cia.algorithm.WaveAlgorithm;
 import com.fit.ducanh.test.ConfigurationOfAnh;
 import com.fit.loader.ProjectLoader;
+import com.fit.loader.tree.Search;
+import com.fit.object.Node;
 import com.fit.object.ProjectNode;
 import com.fit.process.DependencyGeneration;
 import com.fit.process.tostring.IToString;
@@ -27,15 +32,20 @@ public class Preprocess {
 		DependencyGeneration.parse(projectRootNode);
 
 		/** Display dependency tree */
-		IToString displayer = new TreeStrategy(projectRootNode);
-		System.out.println(displayer.getString());
+		// IToString displayer = new TreeStrategy(projectRootNode);
+		// System.out.println(displayer.getString());
 
 		/** Json generation for dependency graph */
 		JsonStrategyForDGraph dGraphJson = new JsonStrategyForDGraph(projectRootNode);
 		Utils.writeContentToFile(dGraphJson.getString(), ConfigurationOfAnh.DEFAULT_JSON_PATH_FOR_DEPENDENCY_GRAPH);
 		NodeDescriptionGeneration nodeDescription = new NodeDescriptionGeneration(projectRootNode);
-		
+
 		/** view dependency graph */
 		Utils.openDefaultBrowser("http://localhost/graph.php");
+
+		/** Test wave algorithm */
+		List<Node> leafNode = Search.searchNode(projectRootNode, new WaveLeafCondition());
+		WaveAlgorithm wave = new WaveAlgorithm(leafNode, Utils.createRandomChangeSet(leafNode, 8));
+		
 	}
 }
